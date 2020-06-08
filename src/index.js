36097068a6c8f1693c9ef40d4b9ebc92665e2d11
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2020-06-09 02:48:09 +0500

    first react gameplay

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+let Greets = ({ name }) => (
+  <div>
+
+    <h1 className="heading">Who am I and What have I done?</h1>
+    <p> Hello From <strong>{name}</strong> , future {1 + 1}x AWS Certified and DevOps Engineer.</p>
+    <ul>
+      <li>Space Student from the Institute of Space Science and Technolgy (ISST), University of Karachi.</li>
+
+      <li>PAIC Cloud Native Computing and 4IR Innovation Leader &amp; Architect Student.</li>
+      <li>My Portfolio: <a href="https://heezjee.surge.sh/">HeeZJee</a> </li>
+      <li>My Skills:
+        <ul >
+          <li >AWS</li>
+          <li>Kubernetes</li>
+          <li>Docker</li>
+          <li>Linux</li>
+          <li>JavaScript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>SCSS</li>
+          <li>Bootstrap</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Greets name={'Hafeez Ghanchi'}></Greets>, document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
